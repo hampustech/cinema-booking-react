18f193a47db5c2fe02d0d3174546333ed908026f
hampustech <hrogestam@proton.me>
2026-02-05 15:53:11 +0100

    added getMovies-function in App.jsx and dropdown

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,32 @@
+import { useState } from "react";
+import { useEffect } from "react";
 import './App.css'
 
 function App() {
+
+  const [movies, setMovies] = useState({});
+
+  useEffect(() => {
+    async function getMovies() {
+      const response = await fetch("http://localhost:3001/movies");
+      const moviesFromServer = await response.json();
+      console.log("Filmer från server:", moviesFromServer);
+      setMovies(moviesFromServer);
+    }
+    getMovies();
+  }, []);
+
   return (
     <>
  <div className="movie-container">
       <label htmlFor="movie">Pick a movie:</label>
-      <select name="movie" id="movie">
-        <option value="100">Fast and furious 6 (100 kr)</option>
-        <option value="50">The mummy returns (50 kr)</option>
-        <option value="70">Jumanji: Welcome to the Jungle (70 kr)</option>
-        <option value="40">Rampage (40 kr)</option>
+      <select id="movie">
+        {Array.isArray(movies) && 
+          movies.map((movie, index) => (
+          <option key={index} value={movie.Price}>
+            {movie.Title} ({movie.Price} kr)
+          </option>
+        ))}
       </select>
     </div>
     <ul className="showcase">
