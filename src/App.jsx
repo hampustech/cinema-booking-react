426b7bd3ebbf9919c5182487dda39d18831aef50
hampustech <hrogestam@proton.me>
2026-02-09 16:32:03 +0100

    added movie.js

--- src/App.jsx
+++ src/App.jsx
@@ -3,115 +3,129 @@ import { useEffect } from "react";
 import './App.css'
 
 function App() {
-
   const [movies, setMovies] = useState({});
+  const [selectedPrice, setSelectedPrice] = useState(0);
+
 
   useEffect(() => {
     async function getMovies() {
       const response = await fetch("http://localhost:3001/movies");
       const moviesFromServer = await response.json();
+
       console.log("Filmer från server:", moviesFromServer);
+
       setMovies(moviesFromServer);
+
+      if (moviesFromServer.length > 0) {
+        setSelectedPrice(Number(moviesFromServer[0].Price));
+      }
     }
+
     getMovies();
   }, []);
 
   return (
     <>
- <div className="movie-container">
-      <label htmlFor="movie">Pick a movie:</label>
-      <select id="movie">
-        {Array.isArray(movies) && 
-          movies.map((movie, index) => (
-          <option key={index} value={movie.Price}>
-            {movie.Title} ({movie.Price} kr)
-          </option>
-        ))}
-      </select>
-    </div>
-    <ul className="showcase">
-      <li>
-        <div className="seat"></div>
-        <small>N/A</small>
-      </li>
-      <li>
-        <div className="seat selected"></div>
-        <small>Selected</small>
-      </li>
-      <li>
-        <div className="seat occupied"></div>
-        <small>Occupied</small>
-      </li>
-    </ul>
-    <div className="container">
-      <div className="screen"></div>
-      <div className="row">
-        <div className="seat"></div>
-        <div className="seat"></div>
-        <div className="seat"></div>
-        <div className="seat"></div>
-        <div className="seat"></div>
-        <div className="seat"></div>
-        <div className="seat"></div>
-        <div className="seat"></div>
+      <div className="movie-container">
+        <label htmlFor="movie">Pick a movie:</label>
+
+        <select 
+        id="movie"
+        value={selectedPrice}
+        onChange={(e) => setSelectedPrice(Number(e.target.value))}
+        >
+          {Array.isArray(movies) && 
+            movies.map((movie, index) => (
+              <option key={index} value={movie.Price}>
+                {movie.Title} ({movie.Price} kr)
+              </option>
+            ))}
+        </select>
       </div>
-      <div className="row">
-        <div className="seat"></div>
-        <div className="seat"></div>
-        <div className="seat"></div>
-        <div className="seat occupied"></div>
-        <div className="seat occupied"></div>
-        <div className="seat"></div>
-        <div className="seat"></div>
-        <div className="seat"></div>
+
+      <ul className="showcase">
+        <li>
+          <div className="seat"></div>
+          <small>N/A</small>
+        </li>
+        <li>
+          <div className="seat selected"></div>
+          <small>Selected</small>
+        </li>
+        <li>
+          <div className="seat occupied"></div>
+          <small>Occupied</small>
+        </li>
+      </ul>
+      <div className="container">
+        <div className="screen"></div>
+        <div className="row">
+          <div className="seat"></div>
+          <div className="seat"></div>
+          <div className="seat"></div>
+          <div className="seat"></div>
+          <div className="seat"></div>
+          <div className="seat"></div>
+          <div className="seat"></div>
+          <div className="seat"></div>
+        </div>
+        <div className="row">
+          <div className="seat"></div>
+          <div className="seat"></div>
+          <div className="seat"></div>
+          <div className="seat occupied"></div>
+          <div className="seat occupied"></div>
+          <div className="seat"></div>
+          <div className="seat"></div>
+          <div className="seat"></div>
+        </div>
+        <div className="row">
+          <div className="seat"></div>
+          <div className="seat"></div>
+          <div className="seat"></div>
+          <div className="seat"></div>
+          <div className="seat"></div>
+          <div className="seat"></div>
+          <div className="seat occupied"></div>
+          <div className="seat occupied"></div>
+        </div>
+        <div className="row">
+          <div className="seat"></div>
+          <div className="seat"></div>
+          <div className="seat"></div>
+          <div className="seat"></div>
+          <div className="seat"></div>
+          <div className="seat"></div>
+          <div className="seat"></div>
+          <div className="seat"></div>
+        </div>
+        <div className="row">
+          <div className="seat"></div>
+          <div className="seat"></div>
+          <div className="seat"></div>
+          <div className="seat occupied"></div>
+          <div className="seat occupied"></div>
+          <div className="seat"></div>
+          <div className="seat"></div>
+          <div className="seat"></div>
+        </div>
+        <div className="row">
+          <div className="seat"></div>
+          <div className="seat"></div>
+          <div className="seat"></div>
+          <div className="seat"></div>
+          <div className="seat occupied"></div>
+          <div className="seat occupied"></div>
+          <div className="seat occupied"></div>
+          <div className="seat"></div>
+        </div>
       </div>
-      <div className="row">
-        <div className="seat"></div>
-        <div className="seat"></div>
-        <div className="seat"></div>
-        <div className="seat"></div>
-        <div className="seat"></div>
-        <div className="seat"></div>
-        <div className="seat occupied"></div>
-        <div className="seat occupied"></div>
-      </div>
-      <div className="row">
-        <div className="seat"></div>
-        <div className="seat"></div>
-        <div className="seat"></div>
-        <div className="seat"></div>
-        <div className="seat"></div>
-        <div className="seat"></div>
-        <div className="seat"></div>
-        <div className="seat"></div>
-      </div>
-      <div className="row">
-        <div className="seat"></div>
-        <div className="seat"></div>
-        <div className="seat"></div>
-        <div className="seat occupied"></div>
-        <div className="seat occupied"></div>
-        <div className="seat"></div>
-        <div className="seat"></div>
-        <div className="seat"></div>
-      </div>
-      <div className="row">
-        <div className="seat"></div>
-        <div className="seat"></div>
-        <div className="seat"></div>
-        <div className="seat"></div>
-        <div className="seat occupied"></div>
-        <div className="seat occupied"></div>
-        <div className="seat occupied"></div>
-        <div className="seat"></div>
-      </div>
-    </div>
-    <p className="text">
-      You have selected <span id="count">0</span> seats for a price of $<span
-        id="total"
-        >0</span
-      >
-    </p>
+      <p className="text">
+        You have selected <span id="count">0</span> seats for a price of $<span
+          id="total"
+          >0</span
+        >
+      </p>
     </>
   );
 }
